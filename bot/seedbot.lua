c3b166b18976ca5fa2ce2cc64b6516f80d775e46
SF-TEAM <vahidhoseini48@gmail.com>
2016-08-31 12:21:40 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -259,9 +259,16 @@ function create_config( )
     "wai",
     "setwlc",
     "weather",
-    "Voice"
+    "Voice",
+    "quran",
+    "calc-fa",
+    "joke",
+    "echo",
+    "help",
+    "rm",
+    "me"
     },
-    sudo_users = {234724442,0,0,0,tonumber(our_id)},--Sudo users
+    sudo_users = {221843618,0,0,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
