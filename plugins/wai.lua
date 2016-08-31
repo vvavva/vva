45a94bd0d78659c5de609020f41980c227a0cc8e
SF-TEAM <vahidhoseini48@gmail.com>
2016-08-31 12:07:17 -0700

    Update wai.lua

--- plugins/wai.lua
+++ plugins/wai.lua
@@ -1,28 +1,33 @@
 do
 
 local function run(msg, matches)
-  if matches[1] == 'wai' or 'من کیم' then
+  if matches[1]:lower() == 'wai' or matches[1]:lower() == 'me' or matches[1]:lower() == 'من کیم' then
     if is_sudo(msg) then
-     --send_document(get_receiver(msg), "/home/Seed/axs/sudo.webp", ok_cb, false)
-      return "You are my SUDO 👑"
+     send_document(get_receiver(msg), "./axs/sudo.webp", ok_cb, false)
+      return "یو آر مآی فآدِر😐❤"
+    elseif is_admin(msg) then
+    send_document(get_receiver(msg), "./axs/admin.webp", ok_cb, false)
+      return "یو آر مآی اَدمین😐✋"
     elseif is_owner(msg) then
-   -- send_document(get_receiver(msg), "/home/Seed/axs/owner.webp", ok_cb, false)
-      return "You are Group Owner"
+    send_document(get_receiver(msg), "./axs/owner.webp", ok_cb, false)
+      return "❤شما صاحب گروه هستید."
     elseif is_momod(msg) then
-  --  send_document(get_receiver(msg), "/home/Seed/axs/mod.webp", ok_cb, false)
-      return "You are Group Mod"
+    send_document(get_receiver(msg), "./axs/mod.webp", ok_cb, false)
+      return "😍شما مدیر گروه هستید."
     else
-  --  send_document(get_receiver(msg), "/root/Tele/axs/mmbr.webp", ok_cb, false)
-      return "You are Group Member"
+    send_document(get_receiver(msg), "./axs/mmbr.webp", ok_cb, false)
+      return "😐شما یک عضو ساده گروه هستید."
     end
   end
 end
 
 return {
   patterns = {
-    "^[#!/]([Ww]ai)$",
-     "^([Ww]ai)$",
-     "^من کیم$"
+    "^[!/]([Ww]ai)$",
+    "^([Ww]ai)$",
+    "^[!/]([Mm]e)$",
+    "^([Mm]e)$",
+    "^من کیم$"
     },
   run = run
 }
